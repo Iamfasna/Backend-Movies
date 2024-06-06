69317ebbe06defed163d196e9d5666796bc80c24
Fasna F F <fasnaff2@gmail.com>
2024-06-06 22:53:29 +0530

    final

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(express.json());
 
 app.use(cors({
-  origin: "*",
+  origin: 'https://cinemakaanuu.netlify.app',
   methods: ["GET", "POST"],
   credentials: true
 }));
